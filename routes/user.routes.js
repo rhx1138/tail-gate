334a0dea38f824d00dcdbda0a5b6ebb66928636b
rhx1138 <ryanjhendrix@gmail.com>
2022-09-13 11:08:17 -0400

    updated routes, added env

---
+++ routes/user.routes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+const users = require("../controllers/user.controller.js");
+const router = express.Router();
+
+// Create a new User
+router.post("/", users.create);
+// Retrieve a single User with id
+router.get("/:id", users.findOne);
+
+module.exports = router;
