0140e80ac6aea4a895531a46510352b6e6842f35
matthew dehart <mattidehart@hotmail.com>
2022-09-14 17:00:15 -0400

    working on profile page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,28 +40,16 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
 
   res.render('login');
 });
 
 router.get('/signup', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-
+  
   res.render('signup');
 });
 
 router.get('/confirm', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
 
   res.render('confirm');
 });
